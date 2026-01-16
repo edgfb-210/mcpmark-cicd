7d63e2080fb59c131655540f035585d7aae70d6a
edgfb-210 <jtrgrce8@605845.xyz>
2026-01-16 12:01:49 +0800

    Add example file with linting violations

---
+++ src/example.js
@@ -0,0 +1,14 @@
+// Example file with linting errors
+
+const greeting = "Hello World"
+console.log(greeting)
+
+let unusedVar = 42
+
+function test() {
+  return "test"
+}
+
+if (true) {
+  console.log("true")
+}
